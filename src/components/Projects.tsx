75a989e087af6a25f553170660029b8232b1e36f
Tales Rodrigues <tales00001@gmail.com>
2023-02-25 10:20:42 -0300

    Projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import projects from '@/utils/projects'
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
+import { BsFillSquareFill, BsSquare, BsCodeSlash } from 'react-icons/bs'
+import Link from 'next/link'
 
 export default function Projects() {
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(0)
+
+  const nextProject = () => {
+    const isLastProject = currentProjectIndex === projects.length - 1
+
+    if (isLastProject) setCurrentProjectIndex(0)
+    else setCurrentProjectIndex(previousIndex => previousIndex + 1)
+  }
+
+  const previousProject = () => {
+    const isFirstProject = currentProjectIndex === 0
+
+    if (isFirstProject) setCurrentProjectIndex(projects.length - 1)
+    else setCurrentProjectIndex(previousIndex => previousIndex - 1)
+  }
+
   return (
-    <main>
-      Projects
-    </main>
+    <section className='min-h-[550px] flex flex-col items-center justify-around px-8'>
+        <h2>{projects[currentProjectIndex].title}</h2>
+        <Image src={projects[currentProjectIndex].image} alt={projects[currentProjectIndex].title} />
+        <p className='text-justify'>{projects[currentProjectIndex].description}</p>
+        <Link className='flex items-center w-1/3 justify-around' href={projects[currentProjectIndex].code} target='_blank'>See Code<BsCodeSlash /></Link>
+      <div className='absolute left-2 translate-y-10' onClick={previousProject}>
+        <AiOutlineLeft />
+      </div>
+      <div className='absolute right-2 translate-y-10' onClick={nextProject}>
+        <AiOutlineRight />
+      </div>
+      <div className='flex justify-between w-1/4'>
+        {projects.map((_project, index) => {
+          if (index === currentProjectIndex) return <BsFillSquareFill key={index} onClick={() => setCurrentProjectIndex(index)} />
+          return <BsSquare key={index} onClick={() => setCurrentProjectIndex(index)} />
+        })}
+      </div>
+    </section>
   )
 }
---
+++ src/utils/projects.ts
@@ -0,0 +1,45 @@
+import who from '../images/projects/who.png'
+import api from '../images/projects/api.png'
+import obs from '../images/projects/obs.png'
+import bank from '../images/projects/bank.png'
+
+const projects = [
+  {
+    title: 'Who Said It?',
+    // link: 'https://who-said-it-talesrodriguesdev.vercel.app/',
+    code: 'https://github.com/talesrodriguesDEV/who-said-it-II',
+    description: 'It\'s a React app built with TypeScript. You can choose a keyword and get a random quote about it. You\'ll also be able to guess "Who said it?" and save your favorite quotes. ',
+    category: 'Frontend',
+    image: who,
+    // date: '',
+  },
+  {
+    title: 'Quotes API',
+    // link: 'https://tales-observatory.vercel.app/',
+    code: 'https://github.com/talesrodriguesDEV/quotes-api',
+    description: 'It\'s a CRUD API about Quotes and Authors built with Node. The goal was to isolate the business rules from the dependencies (Express and MongoDB).',
+    category: 'Backend',
+    image: api,
+    // date: '',
+  },
+  {
+    title: 'Tales\' Observatory',
+    // link: 'https://tales-observatory.vercel.app/',
+    code: 'https://github.com/talesrodriguesDEV/tales-observatory',
+    description: 'It\'s an astronomical observatory built with Next.js based on NASA Image and Video Library API. You can find amazing Universe pictures, access full information about them and save your favorites!',
+    category: 'Frontend',
+    image: obs,
+    // date: '19/01/2023',
+  },
+  {
+    title: 'TR Bank',
+    // link: 'https://github.com/talesrodriguesDEV/trbank',
+    code: 'https://github.com/talesrodriguesDEV/trbank',
+    description: 'It\s a Full Stack Bank built with React, Tailwind CSS, TypeScript, Node, Express, Mongoose, Docker and JWT.',
+    category: 'Full Stack',
+    image: bank,
+    // date: '06/12/2022',
+  }
+]
+
+export default projects
